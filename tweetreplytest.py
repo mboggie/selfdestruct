23e6ec30dc5ea1941adaaab984d955842e420644
Matt Boggie <mboggie@me.com>
2013-04-04 17:59:42 -0400

    refactored to use better twitter llibrary, smarter iterator

--- tweetreplytest.py
+++ tweetreplytest.py
@@ -1,31 +1,36 @@
 # -*- coding: utf-8 -*-
 # May or may not need that up there, just saving it so I know what to do if I ever need unicode or something crazy.
+# Note: Refactoring to use the better-maintained Python Twitter Tools APIs and hooks (https://github.com/sixohsix/twitter)
 
-import twitstream
+from twitter import *
 
-jargonlist = ["touch base"," productize", "table stakes", "game changer", "action item", "take this offline", "time box", "methodology", "solomo", "circle back", "the ask", "creatives", "gamify", "digital space", "mobile space", "plus up", "gamification"]
+#jargonlist = ["touch base"," productize", "table stakes", "game changer", "action item", "take this offline", "time box", "methodology", "solomo", "circle back", "the ask", "creatives", "gamify", "digital space", "mobile space", "plus up", "gamification"]
 
-def callback(status):
+jargonlist = ["longing for", "yearning for"]
+
+def parse_for_jargon(status):
 	keyword = None
-	for word in jargonlist:
-		if word in status.get("text").lower():
-			keyword = word
-	
-	if keyword is None:
-		return
-				
-	#check if this is a straight RT; if so, skip it
-	#check for special cases / regex matches for the word that disqualify it (i.e. "the asking")
-	#insert the user / word if they don't exist, or...
-	#update the count if they do
-	
-	
-	print "%s said a jargon word! (%s) in %s" % (status.get('user', {}).get('screen_name'), keyword, status.get("text"))
-	
 
+	if 'text' in status:
+		for word in jargonlist:
+			if word in status['text'].lower():
+				keyword = word
+		
+		if keyword is None:
+			#print "bzz"
+			return
+					
+		# check if this is a straight RT; if so, skip it
+		# check for special cases / regex matches for the word that disqualify it (i.e. "the asking")
+		# insert the user / word if they don't exist, or...
+		# update the count if they do
 
-
+		print "%s said \"%s\" in %s" % (status['user']['screen_name'], keyword, status['text'])
+	
 #end 
 
-
-twitstream.track("jargonjar", "coldFus10n", callback, jargonlist).run()
+if __name__ == "__main__":
+	twitter_stream = TwitterStream(auth=UserPassAuth("jargonjar", "coldFus10n"))
+	iterator = twitter_stream.statuses.sample()
+	for tweet in iterator:
+		parse_for_jargon(tweet)
