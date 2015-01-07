de6b13d56724547e0196af0e9a97150609d928d9
Matt Boggie <mboggie@mac.com>
2015-01-07 17:21:45 -0500

    POC 0.1: searches for SD5 and parses create/delete times. Need to think next about streams and pattern matching.

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ A service that will monitor authenticated tweet streams and destroy tweets on yo
 ## Status
 
 This service is under development and does not currently work. Come back later.
+
+## Requirements
+
+- Uses the twitter library (`easy_install twitter`) from https://github.com/sixohsix/twitter
+- Expects you to have saved a local secrets.sh with consumer tokens and to have added that to the server's environment (use secrets_sample.sh as a guide)
--- destroy.py
+++ destroy.py
@@ -3,25 +3,41 @@
 # Note: Refactoring to use the better-maintained Python Twitter Tools APIs and hooks (https://github.com/sixohsix/twitter)
 
 from twitter import *
-import datetime
+from datetime import datetime as dt, timedelta
+from dateutil.parser import parse
+import os
 
 jargonlist = ["longing for", "yearning for"]
 
-def schedule(status):
+try:
+	authblock = OAuth(
+	    consumer_key = os.environ["SDAPP_CONSUMER_KEY"],
+	    consumer_secret = os.environ["SDAPP_CONSUMER_SECRET"],
+	    # in future tokens will be addressed programmatically; this is just for now
+	    token = os.environ["SDAPP_ACCESS_KEY"],
+	    token_secret = os.environ["SDAPP_ACCESS_SECRET"]
+	)
+
+except KeyError:
+	print "Please set your \"SDAPP_\" environment variables for Twitter OAuth before running destroy.py."
+
+def schedule(tweet):
 	#simple case: we did the search on SD5 first. start there.
 	
-	createtime = datetime.strptime(status['createdat'])
-	destroytime = createtime + datetime.timedelta(0,5*60)
+	createstring = tweet['created_at']
+	createtime = parse(createstring)
+	destroytime = createtime + timedelta(0,5*60)
 
 	#schedule destruction at destroytime
 
-	print "%s wants to destroy a tweet at  %Z: \n\"%s\"" % (status['user']['screen_name'], destroytime, status['text'])
+	print "%s wants to destroy a tweet at  %s: \n\"%s\"" % (tweet['user']['screen_name'], dt.strftime(destroytime, "%c"), tweet['text'])
 	
 #end schedule 
 
 if __name__ == "__main__":
-	t = Twitter(auth=UserPassAuth("jargonjar", "abcdefg"))
-	iterator = t.search.tweets(q="#sd5")
+	t = Twitter(auth=authblock)
+	search_results = t.search.tweets(q="#sd5")
 
-	for tweet in iterator:
+	for tweet in search_results['statuses']:
 		schedule(tweet)
+		
